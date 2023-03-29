b43ed889dc5af983684167f12c0a01c829e4bed1
s256991 <s256991@studenti.polito.it>
2023-03-29 11:15:11 +0200

    initial_commit

--- agent.py
+++ agent.py
@@ -2,11 +2,13 @@ from langchain import OpenAI
 from tools_creation import tools
 from langchain.agents import initialize_agent
 from API_keys import OPENAI_API_KEY
+from langchain.memory import ConversationBufferMemory
 
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
 
 llm=OpenAI(temperature=0,openai_api_key=OPENAI_API_KEY)
-agent = initialize_agent(tools, llm, agent="zero-shot-react-description", verbose=True)
+agent = initialize_agent(tools, llm, agent="chat-conversational-react-description", verbose=True, memory=memory)
 
 
-
-agent.run("Look for the number of particpants to the poll twitter and multiplied it by 7.")
+answer = agent.run("Hello, how are you?")
+print(answer)
--- test.py
+++ test.py
@@ -1,4 +1,26 @@
+import PyPDF2
+import tabula
+
+def extract_tables_from_pdf(pdf_file_path):
+    # Open the PDF file in read binary mode
+    with open(pdf_file_path, 'rb') as pdf_file:
+        # Create a PDF reader object
+        pdf_reader = PyPDF2.PdfReader(pdf_file)
+
+        # Initialize an empty list to store the extracted tables
+        tables = []
+
+        # Loop through each page in the PDF
+        for page_num in range(len(pdf_reader.pages)):
+            # Extract the page as a DataFrame using tabula-py
+            page_df = tabula.read_pdf(pdf_file_path, pages=page_num+1, pandas_options={'header': None})
+            # Check if the page contains any tables
+            if len(page_df) > 0:
+                # Append the tables to the list
+                tables.extend(page_df)
+
+        # Return the list of tables
+        return tables
 
 
-with open('history.txt', 'a') as f:
-    f.write('\ntest')
+tables = extract_tables_from_pdf('files\REPORT CASE-Academy for Business and Technology (1).pdf')
