d0b801138c72d7d942a2319bfcfecfa8f0f4835b
Bando1805 <93611807+Bando1805@users.noreply.github.com>
2023-04-07 17:14:33 +0200

    Delete API_keys.py

--- API_keys.py
+++
@@ -1,9 +0,0 @@
-import os
-OPENAI_API_KEY = "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"
-PINECONE_API = "2151a2a4-4976-48fa-a36c-06678cc04aa0"
-PINECONE_ENV = "us-east-1-aws"
-WOLFRAM_ALPHA_APPID = "3TUVH9-4H4GXER7KT"
-# os.environ["OPENAI_API_KEY"] = "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"
-# os.environ["SERPER_API_KEY"] = "97e73cccca82781bd1df5f9c77c57e80b25d43da"
-# os.environ["PINECONE_API"]= "2151a2a4-4976-48fa-a36c-06678cc04aa0"
-# os.environ["PINECONE_ENV"]= "us-east-1-aws"
